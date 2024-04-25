baf0ac5b1d6fa1550f847e7aef4c160ece4f612d
NIwnla <sonnguyenvietbao@gmail.com>
2024-04-25 15:37:10 +0700

    Update Student.cs

--- NashTechAssignment/Data/Student.cs
+++ NashTechAssignment/Data/Student.cs
@@ -8,7 +8,7 @@
 		public DateTime? DOB { get; set; }
 		public string? PhoneNumber { get; set; }
 		public string? BirthPlace { get; set; }
-		public int? Age { get; set; }
+		public int? Age { get => DateTime.Now.Year - DOB.Value.Year;}
 		public bool? IsGraduated { get; set; }
 	}
 }
